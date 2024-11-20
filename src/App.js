f0fca01899ea7e0949ac010e3c117a5a3307d04f
Anton Polyakov <polyakov.a.a.03@mail.ru>
2024-11-21 01:27:12 +0700

    Get all heroes from star wars api

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import { Component } from "react";
+import StarWarsHeroe from "./components/StarWarsHeroe";
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			allHeroes: [],
+			isLoading: true,
+			error: "",
+		};
+	}
+
+	componentDidMount() {
+		const apiUrl = "https://swapi.dev/api/people/";
+		this.getAllHeroes(apiUrl);
+	}
+
+	// Получим всех героев со всех страниц рекурсией
+	getAllHeroes(url) {
+		// Фетч на переданный url
+		fetch(url)
+			// проверка ответа
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+				this.setState({
+					error: `Ошибка: ${response.status}`,
+					isLoading: false,
+				});
+				throw new Error(`Не удалось получить ответ, статус: ${response.status}`);
+			})
+			// ответ успешно получен
+			.then((data) => {
+				// обновляем состояние (к имеющимся в состоянии герояем добавляем героев из запроса)
+				this.setState((prevState) => {
+					const newState = {
+						allHeroes: [...prevState.allHeroes, ...data.results],
+					};
+					return newState;
+				});
+				// проверяем, закончились ли страницы с героями
+				if (data.next) {
+					this.getAllHeroes(data.next);
+				} else {
+					console.log("Страниц с героями закончились");
+					this.setState({ isLoading: false });
+				}
+			})
+			.catch((error) => console.log("Возникла ошибка:", error));
+	}
+
+	render() {
+		const { isLoading, error, allHeroes } = this.state;
+		return (
+			<div>
+				{isLoading
+					? "Загрузка данных..." // <Loading />
+					: error
+					? `Не удалось загрузить данные. ${error}` // <Error />
+					: allHeroes.map((heroe, index) => (
+							<StarWarsHeroe key={index} heroe={heroe} />
+					  ))}
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/components/StarWarsHeroe.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function StarWarsHeroe(props) {
+	// console.log(props.heroe);
+	return <div>{props.heroe.name}</div>;
+}
